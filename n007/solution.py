8e3eb4c16e211766a1a8a0a71328f9ae705a7524
yekki <gary.niu@gmail.com>
2020-11-15 01:58:55 +0000

    add 7

---
+++ n007/solution.py
@@ -0,0 +1,10 @@
+INPUT = 3210
+EXPECTED = 123
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x == 0: return x
+        ret = int(str(x)[::-1]) if x >0 else - int(str(-x)[::-1])
+        return ret if ret >= pow(-2, 31) and ret <= pow(2, 31) - 1 else 0
+
+
--- solutions.py
+++ solutions.py
@@ -2,6 +2,7 @@ import unittest
 from common import timer
 from n001.solution import Solution as S1, INPUT as I1, EXPECTED as E1
 from n002.solution import Solution as S2, INPUT as I2, EXPECTED as E2
+from n007.solution import Solution as S7, INPUT as I7, EXPECTED as E7
 from n830.solution import Solution as S830, INPUT as I830, EXPECTED as E830
 
 class SolutionTestCase(unittest.TestCase):
@@ -19,6 +20,12 @@ class SolutionTestCase(unittest.TestCase):
         result = s.addTwoNumbers(I2[0], I2[1])
         self.assertTrue(result == E2)
 
+    @timer
+    def test_7(self):
+        s = S7()
+        result = s.reverse(I7)
+        self.assertEqual(result, E7)
+    
     @timer
     def test_830(self):
         s = S830()
