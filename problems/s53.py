29acea566557185209acdb4e8b6775d3723212bb
yekki <yekki@me.com>
2020-11-17 08:36:02 +0800

    add 53

---
+++ problems/s53.py
@@ -0,0 +1,24 @@
+from common import AbstractSolution
+from typing import List
+
+
+class Solution(AbstractSolution):
+    def maxSubArray(self, nums: List[int]) -> int:
+        if not nums:
+            return None
+
+        res = cur = nums[0]
+        c = len(nums)
+        for i in range(1, c):
+            if cur > 0:
+                cur += nums[i]
+            else:
+                cur = nums[i]
+
+            res = max(res, cur)
+        return res
+
+    def _validate(self, input, expected) -> bool:
+        result = self.maxSubArray(input)
+
+        return result == expected
