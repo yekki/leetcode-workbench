280753f67968d94d1893e34a806fd391f185aa96
yekki <yekki@me.com>
2020-11-20 08:31:28 +0800

    add 171

---
+++ problems/s171.py
@@ -0,0 +1,9 @@
+from . import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def titleToNumber(self, s: str) -> int:
+        pass
+
+    def _validate(self, input, expected) -> bool:
+        pass
