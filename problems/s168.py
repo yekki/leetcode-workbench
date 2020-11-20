54f7c959faf85217aeed5a2ea0ccea65c24fcaa6
进欣 <xiuyuan.nxy@alipay.com>
2020-11-20 12:35:54 +0800

    add 168, 171

---
+++ problems/s168.py
@@ -0,0 +1,25 @@
+from . import AbstractSolution
+
+
+class Solution(AbstractSolution):
+    def convertToTitle(self, n: int) -> str:
+        l = []
+        while (n > 0):
+            if (n % 26 != 0):
+                l.append(n % 26)
+                n = n // 26
+            else:
+                n = n // 26
+                l.append(26)
+                n = n - 1
+        s = ''
+        for i in l[::-1]:
+            s = s + chr(ord('A') + i - 1)
+        return s
+
+        return result
+
+    def _validate(self, input, expected) -> bool:
+        result = self.convertToTitle(input)
+
+        return result == expected
--- problems/s171.py
+++ problems/s171.py
@@ -3,7 +3,14 @@ from . import AbstractSolution
 
 class Solution(AbstractSolution):
     def titleToNumber(self, s: str) -> int:
-        pass
+
+        num = 0
+        for c in s:
+            num = num * 26 + ord(c) - ord("A") + 1
+
+        return num
 
     def _validate(self, input, expected) -> bool:
-        pass
+        result = self.titleToNumber(input)
+
+        return result == expected
