1c1434a6d56fcd2833bb67d9668085e1f064b3ed
进欣 <xiuyuan.nxy@alipay.com>
2020-12-18 14:19:21 +0800

    add 1668 & 1502 & 1512

--- problems/s1512.py
+++ problems/s1512.py
@@ -1,12 +1,12 @@
 from typing import List
 from common import Problem
 
-
+#TODO 不理解
 class Solution(Problem):
     def numIdenticalPairs(self, nums: List[int]) -> int:
         from collections import Counter
         dic = Counter(nums)
-        return sum((value -1)*value // 2 for value in dic.values())
+        return sum((value - 1) * value // 2 for value in dic.values())
 
     def _validate(self, input, expected) -> tuple:
         result = self.numIdenticalPairs(input)
