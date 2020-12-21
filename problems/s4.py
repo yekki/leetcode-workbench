10bdc249d6fa4d97fac8c3e53fed343c2b8c0733
进欣 <xiuyuan.nxy@alipay.com>
2020-12-21 08:31:37 +0800

    add 4

---
+++ problems/s4.py
@@ -0,0 +1,26 @@
+from common import Problem
+from typing import List
+import statistics
+
+class Solution(Problem):
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        return statistics.median(nums1)
+
+    def findMedianSortedArrays_1(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        c = len(nums1)
+        nums1.sort()
+        if c % 2 != 0:
+            return nums1[c//2]
+        else:
+            return (nums1[c//2] + nums1[(c - 1)//2]) / 2
+
+    def _validate(self, input, expected) -> tuple:
+        result = self.findMedianSortedArrays_1(input['p1'], input['p2'])
+
+        return result == expected, result
+
+
+if __name__ == '__main__':
+    Solution.test(__file__)
