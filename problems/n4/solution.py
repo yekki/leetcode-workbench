47f8d1c9cb2a7636fc5ca4b70a412ca9c1e51f3d
进欣 <xiuyuan.nxy@alipay.com>
2021-01-06 09:32:00 +0800

    fix update

--- problems/n4/solution.py
+++ problems/n4/solution.py
@@ -16,6 +16,31 @@ class Solution(Problem):
         else:
             return (nums1[c//2] + nums1[(c - 1)//2]) / 2
 
+    def findMedianSortedArrays_2(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = [0] * (len(nums1) + len(nums2))
+        i, l, r = 0, 0, 0
+
+        while l < len(nums1) and r < len(nums2):
+            if nums1[l] < nums2[r]:
+                nums3[i] = nums1[l]
+                l += 1
+            else:
+                nums3[i] = nums2[r]
+                r += 1
+            i += 1
+
+        if i == len(nums1):
+            nums3[i:] = nums2[r:]
+        else:
+            nums3[i:] = nums2[l:]
+
+        c = len(nums3)
+
+        if c % 2 != 0:
+            return float(nums1[c//2])
+        else:
+            return (nums1[c//2] + nums1[(c - 1)//2]) / 2
+
 
 if __name__ == '__main__':
     Solution.test(__file__)
